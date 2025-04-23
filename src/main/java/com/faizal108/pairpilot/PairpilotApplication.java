c5ac62277a7c5a45e2dabc789d5a455a15417f93
faizal108 <9467378+faizal108@users.noreply.github.com>
2025-04-24 03:06:58 +0530

    project init

---
+++ src/main/java/com/faizal108/pairpilot/PairpilotApplication.java
@@ -0,0 +1,13 @@
+package com.faizal108.pairpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PairpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PairpilotApplication.class, args);
+	}
+
+}
